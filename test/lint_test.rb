806bd116640cc57c5fc010ed6d50005871d4fdfe
Hans Krutzer <git@pixelspaceships.com>
2015-01-19 13:37:12 +0100

    remove circleci

--- test/lint_test.rb
+++ test/lint_test.rb
@@ -1,5 +1,4 @@
 require_relative "../lib/roger_scsslint/lint.rb"
-require_relative "test_helper.rb"
 require "test/unit"
 
 class TesterStub
--- test/test_helper.rb
+++
@@ -1,5 +0,0 @@
-if ENV['CIRCLE_ARTIFACTS']
-  require 'simplecov'
-  dir = File.join("..", "..", "..", ENV['CIRCLE_ARTIFACTS'], "coverage")
-  SimpleCov.coverage_dir(dir)
-end
