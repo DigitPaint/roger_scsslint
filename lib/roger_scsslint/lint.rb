c5e50b3210a5880544602478771959dc841116fd
Edwin van der Graaf <edwin@digitpaint.nl>
2015-03-09 16:40:24 +0100

    Force config file

--- lib/roger_scsslint/lint.rb
+++ lib/roger_scsslint/lint.rb
@@ -8,20 +8,27 @@ module RogerScsslint
   class Lint
     # Configurability is the root of all evil
     # http://fishshell.com/docs/current/design.html#conf
-    def initialize(*)
-      @command = "scss-lint"
-    end
+    def initialize(options = {})
+      @options = {
+        config_file: ".scss-lint.yml",
+        scss_lint: "scss-lint"
+      }
 
-    def detect_scsslint
-      command = [@command, "-v", ">/dev/null"]
-      detect = system(Shellwords.join(command))
-      fail "Could not find linter. Linter should be in Gemfile. " unless detect
+      detect_scsslint
+      @options.update(options) if options
     end
 
     def call(test, _options)
-      detect_scsslint
       test.log(self, "SCSS linting files")
-      system(@command)
+      system(Shellwords.join([@options[:scss_lint], "--config=#{@options[:config_file]}"]))
+    end
+
+    private
+
+    def detect_scsslint
+      command = [@options[:scss_lint], "-v", ">/dev/null"]
+      detect = system(Shellwords.join(command))
+      fail "Could not find linter. Linter should be in Gemfile." unless detect
     end
   end
 end
